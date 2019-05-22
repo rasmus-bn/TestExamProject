119ef895492eabe419a75806e8a163dccad0fbe7
OnkelDunkel <rasbalnor@gmail.com>
2019-05-22 14:08:39 +0200

    basic spring boot

---
+++ src/main/java/dk/projectsbygroup7/testproject/TestProjectApp.java
@@ -0,0 +1,12 @@
+package dk.projectsbygroup7.testproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestProjectApp.class, args);
+    }
+}
